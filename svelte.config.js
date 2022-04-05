343a2a3321e1a221366b05737a3ebb3dfcd91f92
Farseen <oss@itsfarseen.anonaddy.me>
2022-04-06 00:16:48 +0530

    Update assets path

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter(),
-		paths: {base: "/common-time-finder",},
+		paths: {assets: "/common-time-finder",},
 		prerender: {
 			default: true,
 		},
